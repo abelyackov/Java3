670cc2be673ec3393b0987c32d293e6dd4bfd0d4
abelyackov <abelyackov@yandex.ru>
2018-10-02 15:16:58 +0300

    Java3Lesson5

---
+++ Java3Lesson5/untitled7a/src/ru/geekbrains/homework5/Tunnel.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.homework5;
+
+import static ru.geekbrains.homework5.MainClass.semaphore;
+
+public class Tunnel extends Stage {
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            semaphore.acquire();
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release();
+                System.out.println(c.getName() + " закончил этап: " + description);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
